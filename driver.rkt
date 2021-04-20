1327a3ba66b35316f5869efac493c2d9fa1ee292
michaelrauh <michaelrauh@ymail.com>
2021-04-20 17:02:12 -0400

    working version

--- driver.rkt
+++ driver.rkt
@@ -1,7 +1,7 @@
 #lang racket
 
 (require "atom-smasher.rkt" math)
-(struct state (centers next prev boxes phrases raw increment) #:transparent)
+(struct state (lhs-center-to-ortho rhs-center-to-ortho next prev boxes phrases raw increment) #:transparent)
 (provide (struct-out state) (struct-out ortho) drive)
 
 ; assumption - raw is nonempty. Only 2x2 are desired.
@@ -12,11 +12,14 @@
   (define new-next (hash-update (state-next s) prev (λ (s) (set-add s cur)) (set)))
   (define new-prev (hash-update (state-prev s) cur (λ (s) (set-add s prev)) (set)))
   (define increment (make-boxes cur new-next new-prev))
-  (define new-centers (for/fold ([centers (state-centers s)])
-                                ([box increment])
-                        (hash-update centers (calculate-lhs-foreign-center box) (λ (s) (set-add s box)) (set))))
+  (define lhs-center-to-ortho (for/fold ([centers (state-lhs-center-to-ortho s)])
+                                        ([box increment])
+                                (hash-update centers (ortho-lhs-center box) (λ (s) (set-add s box)) (set))))
+  (define rhs-center-to-ortho (for/fold ([centers (state-rhs-center-to-ortho s)])
+                                        ([box increment])
+                                (hash-update centers (ortho-rhs-center box) (λ (s) (set-add s box)) (set))))
   (define new-boxes (hash-update (state-boxes s) '(2 2) (λ (s) (set-union s increment)) (set)))
-  (state new-centers new-next new-prev new-boxes new-phrases new-raw increment))
+  (state lhs-center-to-ortho rhs-center-to-ortho new-next new-prev new-boxes new-phrases new-raw increment))
 
 (define (tails raw)
   (if (= 1 (length raw))
@@ -33,49 +36,76 @@
   (define next #hash(("a" . (set "b" "c")) ("b" . (set "c")) ("c" . (set "d" "b")) ("d" . (set "a"))))
   (define prev #hash(("a" . (set "d")) ("b" . (set "a" "c")) ("c" . (set "a" "b")) ("d" . (set "c"))))
   (define tree (make-phrases '("a" "b" "c" "d" "a" "c" "b")))
-  (define res (drive (state #hash() next prev #hash() tree
+  (define res (drive (state #hash() #hash() next prev #hash() tree
                             '("a" "b" "c" "d" "a" "c" "b") (set)) "d"))
 
-  (check-equal? (state-centers res) (hash
-                                     (array #[#["a"] #["b"]])
-                                     (set
-                                      (ortho
-                                       (array #[#["a" "c"] #["b" "d"]])
-                                       (array #[#["c"] #["d"]])
-                                       (list (set "a") (set "b" "c") (set "d"))))
-                                     (array #[#["a"] #["c"]])
-                                     (set
-                                      (ortho
-                                       (array #[#["a" "b"] #["c" "d"]])
-                                       (array #[#["b"] #["d"]])
-                                       (list (set "a") (set "b" "c") (set "d"))))))
-  (check-equal? (state-next res) #hash(("a" . (set "b" "c"))
-                                       ("b" . ("d" set "c"))
-                                       ("c" . (set "d" "b"))
-                                       ("d" . (set "a"))))
-  (check-equal? (state-prev res) #hash(("a" . (set "d"))
-                                       ("b" . (set "a" "c"))
-                                       ("c" . (set "a" "b"))
-                                       ("d" . ("b" set "c"))))
-  (check-equal? (state-boxes res) (hash
-                                   '(2 2)
-                                   (set
-                                    (ortho
-                                     (array #[#["a" "b"] #["c" "d"]])
-                                     (array #[#["b"] #["d"]])
-                                     (list (set "a") (set "b" "c") (set "d")))
-                                    (ortho
-                                     (array #[#["a" "c"] #["b" "d"]])
-                                     (array #[#["c"] #["d"]])
-                                     (list (set "a") (set "b" "c") (set "d"))))))
+  (check-equal? (state-lhs-center-to-ortho res)
+                (hash
+                 (array #[#["a"] #["b"]])
+                 (set
+                  (ortho
+                   (array #[#["a" "c"] #["b" "d"]])
+                   (array #[#["a"] #["b"]])
+                   (array #[#["c"] #["d"]])
+                   (list (set "a") (set "b" "c") (set "d"))))
+                 (array #[#["a"] #["c"]])
+                 (set
+                  (ortho
+                   (array #[#["a" "b"] #["c" "d"]])
+                   (array #[#["a"] #["c"]])
+                   (array #[#["b"] #["d"]])
+                   (list (set "a") (set "b" "c") (set "d"))))))
+  (check-equal? (state-rhs-center-to-ortho res)
+                (hash
+                 (array #[#["c"] #["d"]])
+                 (set
+                  (ortho
+                   (array #[#["a" "c"] #["b" "d"]])
+                   (array #[#["a"] #["b"]])
+                   (array #[#["c"] #["d"]])
+                   (list (set "a") (set "b" "c") (set "d"))))
+                 (array #[#["b"] #["d"]])
+                 (set
+                  (ortho
+                   (array #[#["a" "b"] #["c" "d"]])
+                   (array #[#["a"] #["c"]])
+                   (array #[#["b"] #["d"]])
+                   (list (set "a") (set "b" "c") (set "d"))))))
+  (check-equal? (state-next res)
+                #hash(("a" . (set "b" "c"))
+                      ("b" . ("d" set "c"))
+                      ("c" . (set "d" "b"))
+                      ("d" . (set "a"))))
+  (check-equal? (state-prev res)
+                #hash(("a" . (set "d"))
+                      ("b" . (set "a" "c"))
+                      ("c" . (set "a" "b"))
+                      ("d" . ("b" set "c"))))
+  (check-equal? (state-boxes res)
+                (hash
+                 '(2 2)
+                 (set
+                  (ortho
+                   (array #[#["a" "b"] #["c" "d"]])
+                   (array #[#["a"] #["c"]])
+                   (array #[#["b"] #["d"]])
+                   (list (set "a") (set "b" "c") (set "d")))
+                  (ortho
+                   (array #[#["a" "c"] #["b" "d"]])
+                   (array #[#["a"] #["b"]])
+                   (array #[#["c"] #["d"]])
+                   (list (set "a") (set "b" "c") (set "d"))))))
   (check-true (set-member? (state-phrases res) (list "a" "b" "c" "d" "a" "c" "b" "d")))
   (check-equal? (state-raw res) '("a" "b" "c" "d" "a" "c" "b" "d"))
-  (check-equal? (state-increment res) (set
-                                       (ortho
-                                        (array #[#["a" "b"] #["c" "d"]])
-                                        (array #[#["b"] #["d"]])
-                                        (list (set "a") (set "b" "c") (set "d")))
-                                       (ortho
-                                        (array #[#["a" "c"] #["b" "d"]])
-                                        (array #[#["c"] #["d"]])
-                                        (list (set "a") (set "b" "c") (set "d"))))))
+  (check-equal? (state-increment res)
+                (set
+                 (ortho
+                  (array #[#["a" "b"] #["c" "d"]])
+                  (array #[#["a"] #["c"]])
+                  (array #[#["b"] #["d"]])
+                  (list (set "a") (set "b" "c") (set "d")))
+                 (ortho
+                  (array #[#["a" "c"] #["b" "d"]])
+                  (array #[#["a"] #["b"]])
+                  (array #[#["c"] #["d"]])
+                  (list (set "a") (set "b" "c") (set "d"))))))
--- safe-driver.rkt
+++ safe-driver.rkt
@@ -5,8 +5,8 @@
 ; assumption - stringbreakingpoint does not occur in any real text. Phrases terminate at text breaks. Broken phrases are not desired in output.
 (define (safe-drive s cur)
   (cond
-    [(equal? "stringbreakingpoint" cur) (state (state-centers s) (state-next s) (state-prev s) (state-boxes s) (state-phrases s) (list) (set))]
-    [(empty? (state-raw s)) (state (state-centers s) (state-next s) (state-prev s) (state-boxes s) (state-phrases s) (append (state-raw s) (list cur)) (set))]
+    [(equal? "stringbreakingpoint" cur) (state (state-lhs-center-to-ortho s) (state-rhs-center-to-ortho s) (state-next s) (state-prev s) (state-boxes s) (state-phrases s) (list) (set))]
+    [(empty? (state-raw s)) (state (state-lhs-center-to-ortho s) (state-rhs-center-to-ortho s) (state-next s) (state-prev s) (state-boxes s) (state-phrases s) (append (state-raw s) (list cur)) (set))]
     [else (process s cur)]))
 
 (define (process s cur)
@@ -26,7 +26,6 @@
   (sift s work-queue))
 
 (define (sift s work-queue)
-  (displayln work-queue)
   (if (empty? work-queue)
       s
       (let ([step (drive-generic s (car work-queue))])
@@ -64,22 +63,24 @@
   (cond
     [(empty? input-strings) state]
     [else (begin
+            (displayln (car input-strings))
             (define step (safe-drive state (car input-strings)))
             (calculate (cdr input-strings) step))]))
 
 (define (make-empty-state)
-  (state #hash() #hash() #hash() #hash() (set) (list) (set)))
+  (state #hash() #hash() #hash() #hash() #hash() (set) (list) (set)))
 
-; a b c  g h i
-; d e f  j k l
+;(define wow (calculate (input-from-file) (make-empty-state)))
 
-; TODO figure out why the above is not found.
-; a b   g h
-; d e   j k
-; 2x2x2 is missing the above entry so the expand fails.
-; hypothesis: the above 2x2x2 is missing because g h j k comes second and only ever appears on the left hand side.
-; This is an issue in both up and over transforms.
-; TODO make both up and over transforms attempt to combine from LHS and RHS
-(define s (calculate (input-strings "a b c d e f g h i j k l a d b e c f g j h k i l a g b h c i d j e k f") (make-empty-state)))
-(display (hash-ref (state-boxes s) '(2 2 2)))
-(void (calculate (input-strings "l") s))
+(module+ test
+  ; a b c  g h i
+  ; d e f  j k l
+  
+  (require rackunit)
+  (define final-ortho (ortho (array #[#[#["a" "b" "c"] #["d" "e" "f"]] #[#["g" "h" "i"] #["j" "k" "l"]]]) (array #[#[#["a" "b"] #["d" "e"]] #[#["g" "h"] #["j" "k"]]]) (array #[#[#["b" "c"] #["e" "f"]] #[#["h" "i"] #["k" "l"]]]) (list (set "a") (set "d" "b" "g") (set "h" "c" "e" "j") (set "k" "i" "f") (set "l"))))
+  (define s (calculate (input-strings "a b c d e f g h i j k l a d b e c f g j h k i l a g b h c i d j e k f") (make-empty-state)))
+  (define ans-one (hash-ref (state-boxes s) '(2 2 3)))
+  (check-false (set-member? ans-one final-ortho))
+  (define final-state (calculate (input-strings "l") s))
+  (define ans-two (hash-ref (state-boxes final-state) '(2 2 3)))
+  (check-true (set-member? ans-two final-ortho)))
