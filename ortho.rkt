f5782fc6d75bed31cc5e759501b7e8f68eb54e58
michaelrauh <michaelrauh@ymail.com>
2021-11-24 11:29:06 -0500

    build out ortho

--- ortho.rkt
+++ ortho.rkt
@@ -1,19 +1,47 @@
 #lang racket
-(provide get-origin make-ortho ortho-size ortho-origin ortho-hops)
 
-(define (get-origin o)
-  1)
+(require rebellion/collection/multiset threading)
+(provide make-ortho ortho-size ortho-origin ortho-hops)
 
-(define (make-ortho a b c d)
-  1)
-
-(define (ortho-size o)
-  1)
+(struct node (name location)
+  #:methods
+  gen:equal+hash
+  [(define (equal-proc a b equal?-recur)
+     (and (equal?-recur (node-name a) (node-name b))
+          (equal?-recur (node-location a) (node-location b))))
+   (define (hash-proc a hash-recur)
+     (+ (hash-recur (node-name a))
+        (* 3 (hash-recur (node-location a)))))
+   (define (hash2-proc a hash2-recur)
+     (+ (hash2-recur (node-name a))
+        (hash2-recur (node-location a))))])
 
 (define (ortho-origin o)
-  1)
+  (~>
+   o
+   (car)
+   (set-first)
+   (node-name)))
+
+(define (make-ortho a b c d)
+  (list
+   (set (node a (multiset)))
+   (set (node b (multiset b)) (node c (multiset c)))
+   (set (node d (multiset b c)))))
+
+(define (ortho-size o)
+  (define corner (set-first (last o)))
+  (define numbers (hash-values (multiset-frequencies (node-location corner))))
+  (apply multiset numbers))
 
 (define (ortho-hops o)
-  (list 1 1))
+  (apply set (set-map (cadr o) node-name)))
 
-; size is a multiset of numbers. Length of each axis. This is the occurrence set of the path to the bottom right corner.
+(module+ test
+  (require rackunit)
+  (define ortho (make-ortho "a" "b" "c" "d"))
+  (define ortho2 (make-ortho "a" "c" "b" "d"))
+  (check-equal? ortho ortho2)
+  (check-equal? (ortho-origin ortho) "a")
+  (check-equal? (ortho-size ortho) (multiset 1 1))
+  (check-equal? (ortho-hops ortho) (set "b" "c")))
--- up.rkt
+++ up.rkt
@@ -2,6 +2,6 @@
 (require "config.rkt" "ortho.rkt" "repo.rkt" threading)
 
 (define (fold-up config repo ortho)
-  (define potential-forwards (find-by-size-and-origin repo (ortho-size ortho) (project-forward config (get-origin ortho))))
-  (define potential-backwards (find-by-size-and-origin repo (ortho-size ortho) (project-backward config (get-origin ortho))))
+  (define potential-forwards (find-by-size-and-origin repo (ortho-size ortho) (project-forward config (ortho-origin ortho))))
+  (define potential-backwards (find-by-size-and-origin repo (ortho-size ortho) (project-backward config (ortho-origin ortho))))
   1)
